92109b33024154d9385768c974bb7d75e315617c
Shohei Aoki <shoaok@gmail.com>
2024-02-20 13:52:36 +0300

    update menu structure

--- assets/src/app.tsx
+++ assets/src/app.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import { render } from 'react-dom'
 import Teams from './components/Teams'
 import Teams2023 from './components/Teams2023'
+import App from './components/Navbar'
  
 const teamsDOM = document.getElementById('teams')
 const teams2023DOM = document.getElementById('teams2023')
+const navbarDOM = document.getElementById('navbar')
 
 if (teamsDOM !== null) render(<Teams />, teamsDOM)
 if (teams2023DOM !== null) render(<Teams2023 />, teams2023DOM)
+if (navbarDOM !== null) render(<App />, navbarDOM)
---
+++ assets/src/components/Navbar.tsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { Navbar, Nav } from "react-bootstrap";
+
+class App extends Component {
+  render() {
+    return (
+      <section className="container">
+        <Navbar
+          className="navbar"
+          role="navigation"
+          aria-label="main navigation"
+        >
+          <div className="navbar-brand">
+            <Nav.Link className="navbar-item" href="/">
+              <img src="image/dojo.png" />
+            </Nav.Link>
+
+            <Nav.Link
+              role="button"
+              className="navbar-burger"
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navbarBasicExample"
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </Nav.Link>
+          </div>
+          <Nav id="navbarBasicExample" className="navbar-menu navbar-end">
+            <Nav.Link className="navbar-item" href="about.html">
+              About
+            </Nav.Link>
+            <Nav.Link className="navbar-item" href="member.html">
+              Member
+            </Nav.Link>
+            <Nav.Link className="navbar-item" href="training.html">
+              Training
+            </Nav.Link>
+            <div className="navbar-item had-dropdown is-hoverable" style={{padding: '0'}}>
+              <div className="navbar-link">Competition</div>
+              <div className="navbar-dropdown">
+                <Nav.Link className="navbar-item" href="competition2023.html">2023</Nav.Link>
+                <Nav.Link className="navbar-item" href="competition2022.html">2022</Nav.Link>
+              </div>
+            </div>
+          </Nav>
+        </Navbar>
+      </section>
+    );
+  }
+}
+export default App;
