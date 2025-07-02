b367cf040d2ba297e834ae04b99140eecf2474d4
Shohei Aoki <shoaok@gmail.com>
2025-07-02 15:48:51 +0300

    update affiliation

--- assets/src/components/Members.tsx
+++ assets/src/components/Members.tsx
@@ -26,33 +26,33 @@ const Members = () => {
       comment: "Interns 2025",
       leader: {
         name: "Victoria Rotich",
-        department: "JKUAT BSc, MT 4th",
+        department: "JKUAT BSc, TIE 4th",
         image: "2025/victoria",
       },
       members: [
         {
           name: "Caleb Wambua",
-          department: "JKUAT BSc, MT 4th",
+          department: "JKUAT BSc, COIN 3rd",
           image: "2025/caleb",
         },
         {
           name: "Joshua Njau",
-          department: "JKUAT BSc, MT 4th",
+          department: "JKUAT BSc, MT 3rd",
           image: "2025/joshua",
         },
         {
           name: "Fiona Opiyo",
-          department: "JKUAT BSc, MT 4th",
+          department: "JKUAT BSc, TIE 4th",
           image: "2025/fiona",
         },
         {
           name: "Susan Wanjiru Kimani",
-          department: "JKUAT BSc, MT 4th",
+          department: "JKUAT BSc, ECE 4th",
           image: "2025/ruth",
         },
         {
           name: "Mutwa Maryanne Farida",
-          department: "JKUAT BSc, MT 4th",
+          department: "JKUAT BSc, ECE 4th",
           image: "2025/farida",
         },
         {
@@ -67,18 +67,18 @@ const Members = () => {
       comment: "Interns 2024",
       leader: {
         name: "Lenny Ng'ang'a",
-        department: "JKUAT BSc, MT 4th",
+        department: "JKUAT BSc, COIN 4th",
         image: "2024/lenny",
       },
       members: [
         {
           name: "Brian Macharia",
-          department: "JKUAT BSc, MT 4th",
+          department: "JKUAT BSc, MT 3rd",
           image: "2024/brian_macharia",
         },
         {
           name: "Kennedy Wahome",
-          department: "JKUAT BSc, MT 4th",
+          department: "JKUAT BSc, ECE 4th",
           image: "2024/kennedy_wahome",
         },
         {
@@ -108,28 +108,28 @@ const Members = () => {
       comment: "Interns 2023",
       leader: {
         name: "Lenny Ng'ang'a",
-        department: "JKUAT BSc, MT 4th",
+        department: "JKUAT BSc, COIN 3rd",
         image: "2024/lenny",
       },
       members: [
         {
           name: "Kennedy Wahome",
-          department: "JKUAT BSc, MT 4th",
+          department: "JKUAT BSc, ECE 3rd",
           image: "2023/kennedy_wahome",
         },
         {
           name: "Judy Wangechi",
-          department: "JKUAT BSc, MT 4th",
+          department: "JKUAT BSc, ECE 3rd",
           image: "2023/judy_wangechi",
         },
         {
           name: "Mirriam Mogeni",
-          department: "JKUAT BSc, MT 4th",
+          department: "JKUAT BSc, ECE 3rd",
           image: "2023/mirriam_mogeni",
         },
         {
           name: "Faith Ngina",
-          department: "JKUAT BSc, MT 4th",
+          department: "JKUAT BSc, ECE 3rd",
           image: "2023/faith_ngina",
         },
         {
@@ -139,7 +139,7 @@ const Members = () => {
         },
         {
           name: "Mwaura Mbugua",
-          department: "JKUAT BSc, MT 4th",
+          department: "JKUAT BSc, EEE 3rd",
           image: "2023/mwaura_mbugua",
         },
       ],
